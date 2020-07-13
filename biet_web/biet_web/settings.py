65639fbb9dd7410a5d4054e642918d32074973e9
root <root@server.bietdvg.edu>
2020-07-12 22:35:33 -0600

    hosted first commit

--- biet_web/biet_web/settings.py
+++ biet_web/biet_web/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'biet_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME','bietdb'),
+        'USER': os.environ.get('DB_USER','bietda'),
+        'PASSWORD': os.environ.get('DB_PASS','rootadmin@1234@'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -130,7 +134,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'assests')
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
